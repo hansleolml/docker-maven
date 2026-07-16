a132d64df2f4b35230358ece25d50532b363b00e
leo <leo@MacBook-Pro-de-Hans.local>
2026-07-15 21:59:21 -0500

    primer commit

---
+++ src/main/java/com/ejemplo/hellosaludo/HelloSaludoApplication.java
@@ -0,0 +1,12 @@
+package com.ejemplo.hellosaludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSaludoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSaludoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ejemplo/hellosaludo/SaludoController.java
@@ -0,0 +1,13 @@
+package com.ejemplo.hellosaludo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "Hello World";
+    }
+}
